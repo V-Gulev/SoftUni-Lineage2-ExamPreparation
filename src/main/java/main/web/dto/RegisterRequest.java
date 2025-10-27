70657f43431a584f701253afad03c9a843b4c312
V-Gulev <velizargulev@abv.bg>
2025-10-27 21:01:12 +0200

    Lineage2 Login and Register Request

---
+++ src/main/java/main/web/dto/RegisterRequest.java
@@ -0,0 +1,26 @@
+package main.web.dto;
+
+
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    @NotBlank
+    @Size(min = 4, max = 16)
+    private String username;
+
+    @NotBlank
+    @Size(min = 4, max = 16)
+    private String password;
+
+    @NotBlank
+    @Size(min = 4, max = 16)
+    private String nickname;
+}
